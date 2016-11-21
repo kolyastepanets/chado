4801b406761ae62535ac13819e12640058a2ba48
Nikolay <kolyastepanets@gmail.com>
2016-11-22 00:57:26 +0200

    views

--- spec/factories/perfomances.rb
+++ spec/factories/perfomances.rb
@@ -2,6 +2,6 @@ FactoryGirl.define do
   factory :perfomance do
     title { ["Бумажное шоу", "Шоу фокусов", "Шоу мыльных пузырей", "Дрессированные животные", "Научное шоу"].sample }
     text { Faker::Lorem.paragraph }
-    image "MyString"
+    image File.new("#{Rails.root}/spec/support/images/logo.png")
   end
 end
