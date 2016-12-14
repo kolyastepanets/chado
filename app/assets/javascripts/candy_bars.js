6735ea9ec5fe556111efba24ba40ddf65eb30c05
Nikolay <kolyastepanets@gmail.com>
2016-12-14 23:37:42 +0200

    preview text, devise

--- app/assets/javascripts/candy_bars.js
+++ app/assets/javascripts/candy_bars.js
@@ -1,11 +1,2 @@
 document.addEventListener("turbolinks:load", function() {
-  $('#imageGallery').lightSlider({
-    gallery:true,
-    item:1,
-    loop:true,
-    thumbItem:9,
-    slideMargin:0,
-    enableDrag: false,
-    currentPagerPosition:'left'
-  });
 })
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161210144908) do
+ActiveRecord::Schema.define(version: 20161214195716) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,8 +39,9 @@ ActiveRecord::Schema.define(version: 20161210144908) do
     t.string   "title"
     t.text     "text"
     t.string   "image"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.text     "preview_text"
   end
 
   create_table "programs", force: :cascade do |t|
